1fee694b58f4c71c7bb4d1d591063b15613f85f7
zarathoustra23 <trajairus@gmail.com>
2024-02-17 14:43:08 +0000

    deuxieme depot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accueil', component:AccueilComponent},
+  {path:'portfolio', component:PortfolioComponent},
+  {path:'resume', component:ResumeComponent},
+  {path:'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    AccueilComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
